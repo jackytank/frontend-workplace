6d0e91beca46913ae86a9e719ad457eec546eaa9
To Minh Tri <balisongian@gmail.com>
2023-08-26 11:19:24 +0000

    angular todo add environment config

--- angular-todo-app/src/app/app.module.ts
+++ angular-todo-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-todo-app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBQGYOebSKKV0plOq_KDeKeDzJsTgeluFM",
+        authDomain: "angular-todo-app-dc3d2.firebaseapp.com",
+        projectId: "angular-todo-app-dc3d2",
+        storageBucket: "angular-todo-app-dc3d2.appspot.com",
+        messagingSenderId: "907134347076",
+        appId: "1:907134347076:web:482afd5f04576fc28b5911",
+        measurementId: "G-N9RW77K00P"
+    }
+};
