934a03dd241138e07c3825259268dbac0d50a8cd
To Minh Tri <balisongian@gmail.com>
2023-08-26 11:55:55 +0000

    reinit angular-todo-app

--- angular-todo-app/src/app/app.module.ts
+++ angular-todo-app/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
