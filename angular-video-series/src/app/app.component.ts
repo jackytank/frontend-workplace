9fb38d1b731f7cf9efdfdbf71ea606c6fb6e13a7
To Minh Tri <balisongian@gmail.com>
2023-08-26 07:03:49 +0000

    create a angular learning project

---
+++ angular-video-series/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub: any;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('interval fired');
+    }, 1000);
+  }
+  ngOnDestroy(): void {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+}
