a83343926a4147d5acf4aaa07792ed5caff615bd
Tri Minh To <tri.minhto@hitachivantara.com>
2023-09-14 16:14:39 +0700

    a

--- learn-ant-design/src/routes/MainLayout.tsx
+++ learn-ant-design/src/routes/MainLayout.tsx
@@ -40,7 +40,7 @@ const items: MenuItem[] = [
             getItem("List", "6", <UnorderedListOutlined />, "projects/list", undefined),
         ]),
     getItem("PlayGround", "7", <QuestionCircleOutlined />, "/playground", undefined),
-    getItem("PlayGround", "8", <PieChartOutlined />, "/powerbi-playground", undefined),
+    getItem("PowerBI", "8", <PieChartOutlined />, "/powerbi-playground", undefined),
 ];
 
 const MainLayout = () => {
--- learn-ant-design/src/routes/PowerBiPlayGround.tsx
+++ learn-ant-design/src/routes/PowerBiPlayGround.tsx
@@ -95,8 +95,15 @@ function PowerBiPlayGround() {
 
   return (
     <div>
-      {header}
+      {/* <span style={{margin:'0 auto'}}>{header}</span> */}
 
+      <iframe
+        title="tri_visual"
+        width="1500"
+        height="750"
+        src="https://app.powerbi.com/reportEmbed?reportId=8192d0bf-6982-4785-abba-7d32b9850c8b&autoAuth=true&ctid=18791e17-6159-4f52-a8d4-de814ca8284a"
+      />
+      <hr />
       <PowerBIEmbed
         embedConfig={sampleReportConfig}
         eventHandlers={eventHandlersMap}
@@ -115,12 +122,6 @@ function PowerBiPlayGround() {
 
       {controlButtons}
 
-      <iframe
-        title="tri_visual"
-        width="1140"
-        height="541.25"
-        src="https://app.powerbi.com/reportEmbed?reportId=8192d0bf-6982-4785-abba-7d32b9850c8b&autoAuth=true&ctid=18791e17-6159-4f52-a8d4-de814ca8284a"
-      />
 
 
     </div>
