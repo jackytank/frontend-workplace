e841b389b0a94e473ead48a58d5cfc5fac8315d9
jt <balisongian@gmail.com>
2023-10-12 23:29:02 +0700

    a

--- learn-ant-design/src/Config.ts
+++ learn-ant-design/src/Config.ts
@@ -1,5 +1,4 @@
 export const Config = {
-    // REACT_APP_API_URL: 'https://64d87c1a5f9bf5b879ce4694.mockapi.io/api/v1',
     REACT_APP_API_URL: 'http://localhost:3333',
     API_PATH: {
         EMPLOYEE: '/employees',
