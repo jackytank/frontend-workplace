73953e74513dcf68d10bda486741d8a4df9c754f
jt <balisongian@gmail.com>
2023-12-08 22:39:03 +0700

    add 5 tests playground.test.tsx

--- learn-ant-design/src/routes/playground/playground.test.tsx
+++ learn-ant-design/src/routes/playground/playground.test.tsx
@@ -1,11 +1,49 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Playground from './playground';
 
-// start testing playground.tsx
-describe('Playground', () => {
-  test('renders Playground component', () => {
-    render(<Playground />);
-    // assert something to make sure jest is working
-    expect(true).toBeTruthy();
-  });
+test('renders Playground component', () => {
+  render(<Playground />);
+});
+
+test('"Demo select confirm" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/Demo select confirm/i);
+  expect(label).toBeInTheDocument();
+});
+
+test('"Demo iframe PowerBI" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/Demo iframe PowerBI/i);
+  expect(label).toBeInTheDocument();
+});
+
+test('"Demo search advanced 2 styles" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/Demo search advanced 2 styles/i);
+  expect(label).toBeInTheDocument();
+});
+
+test('"Learn Fetching REST" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/Learn Fetching REST/i);
+  expect(label).toBeInTheDocument();
+});
+
+test('"React hooks" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/React hooks/i);
+  expect(label).toBeInTheDocument();
+});
+
+test('"Styling" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/Styling/i);
+  expect(label).toBeInTheDocument();
+});
+
+test('"Other" is in the document', () => {
+  const { getByText } = render(<Playground />);
+  const label = getByText(/Other/i);
+  expect(label).toBeInTheDocument();
 });
