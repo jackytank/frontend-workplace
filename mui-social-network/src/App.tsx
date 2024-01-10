a88868c5d182bdd2833ce164cb903ac2ea9ea21d
Tri Minh To <tri.minhto@hitachivantara.com>
2024-01-10 12:43:58 +0700

    a

--- mui-social-network/src/App.tsx
+++ mui-social-network/src/App.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@mui/material";
+import { ClearOutlined, DeleteOutline } from '@mui/icons-material';
+import { Button, IconButton } from '@mui/material';
+import React from 'react';
 
 const App = () => {
   return (
     <div>
-      
+      <Button variant='contained' color='error' size='small' startIcon={<DeleteOutline />}>
+        Delete
+      </Button>
+      <IconButton>
+        <DeleteOutline />
+      </IconButton>
     </div>
   );
 };
