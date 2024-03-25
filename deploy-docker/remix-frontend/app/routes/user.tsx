aaa4803afef466a1dacf2255661d28beb360cee2
Tri Minh To <tri.minhto@hitachivantara.com>
2024-03-25 17:43:13 +0700

    asd

--- deploy-docker/remix-frontend/app/routes/user.tsx
+++ deploy-docker/remix-frontend/app/routes/user.tsx
@@ -1,17 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
-import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs, json, LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import axios from 'axios';
+
+const HOST = 'http://localhost:8080';
+const actionName = '_action';
+enum ReqEnum {
+    CREATE = 'create',
+    DELETE = 'delete'
+}
 
 export const loader = async ({
     request
 }: LoaderFunctionArgs) => {
-    const res = await fetch('http://localhost:8080/api/v1/users/1');
-    const data: UserType = await res.json();
-    console.log('data', data);
+    const res = await fetch(`${HOST}/api/v1/users`);
+    const data: UserType[] = await res.json();
     return json({
-        user: data
+        users: data
     });
 };
 
@@ -19,37 +25,61 @@ export const action = async ({
     request
 }: ActionFunctionArgs) => {
     const formData = await request.formData();
-    // send PUT request to update via axios
-    await axios.put('http://localhost:8080/api/v1/users/1', {
-        name: formData.get('name'),
-        email: formData.get('email')
-    });
+    const { _action, ...values } = Object.fromEntries(formData);
+    console.log('_action', _action);
+    if (_action === ReqEnum.CREATE) {
+        await axios.post(`${HOST}/api/v1/users`, values);
+    } else if (_action === ReqEnum.DELETE) {
+        await axios.delete(`${HOST}/api/v1/users/${values.id}`);
+    }
     return null;
 };
 
 
 const User = () => {
     // get loader data with types
-    const { user } = useLoaderData<typeof loader>();
+    const { users } = useLoaderData<typeof loader>();
 
     return (
-        <div>
-            <h1>Form</h1>
-            <Form method="post" action="/user">
-                <h1>Settings for {user.name}</h1>
-                <input
-                    name="name"
-                    defaultValue={user.name}
-                />
-                <br />
-                <input
-                    name="email"
-                    defaultValue={user.email}
-                />
-                <br />
-                <button type="submit">Save</button>
-            </Form>
-        </div>
+        <main>
+            <h1>Users</h1>
+            {users.length ? (
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>
+                            {user.name} | {user.email} {' '}
+                            <Form
+                                style={{ display: 'inline' }}
+                                method="post"
+                            >
+                                <input type="hidden" name="id" value={user.id} />
+                                <button
+                                    type="submit"
+                                    name={actionName}
+                                    value={ReqEnum.DELETE}
+                                >
+                                    X
+                                </button>
+                            </Form>
+                        </li>
+                    ))}
+                    <li>
+                        <Form method="post">
+                            <input type="text" name="name" placeholder='name...' />
+                            <input type="email" name="email" placeholder='email...' />
+                            <button
+                                type="submit"
+                                name={actionName}
+                                value={ReqEnum.CREATE}
+                            >
+                                Add
+                            </button>
+                        </Form>
+                    </li>
+                </ul>
+            ) : null}
+        </main>
+
     );
 };
 
