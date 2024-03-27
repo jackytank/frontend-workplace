a688945f2e6ea63009e7c8b6d652de4b33993b63
jt <balisongian@gmail.com>
2024-03-28 00:07:19 +0700

    asd

--- deploy-docker/springboot-backend/src/main/java/com/examplebe/demo/user/UserEntities.java
+++ deploy-docker/springboot-backend/src/main/java/com/examplebe/demo/user/UserEntities.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -21,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-public class User {
+class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,4 +34,42 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private Set<UserRole> userRoles;
+}
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private Set<UserRole> userRoles;
+}
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_roles")
+class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
+    private Role role;
+
 }
--- deploy-docker/springboot-backend/src/main/java/com/examplebe/demo/user/UserRepo.java
+++ deploy-docker/springboot-backend/src/main/java/com/examplebe/demo/user/UserRepo.java
@@ -2,6 +2,6 @@ package com.examplebe.demo.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long>{
+public interface UserRepo extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 }
