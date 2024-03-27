759d817e7babbc08bcd8a6b669c8a87caa6dade0
jt <balisongian@gmail.com>
2024-03-28 00:02:03 +0700

    asd

---
+++ deploy-docker/springboot-backend/src/main/java/com/examplebe/demo/IndexController.java
@@ -0,0 +1,13 @@
+package com.examplebe.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        // redirect to swagger ui
+        return "redirect:/swagger-ui.html";
+    }
+}
