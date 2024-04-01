0257720f485f44febd8174c183935a608f487b2a
Tri Minh To <tri.minhto@hitachivantara.com>
2024-04-01 16:44:36 +0700

    asd

--- deploy-docker/next-frontend/src/app/user/page.tsx
+++ deploy-docker/next-frontend/src/app/user/page.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
 
-async function getUsers() {
-  const res = await fetch('http://localhost:8080/users');
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  return res.json();
-}
-
 const UserPage = async () => {
-  const data: UserType[] = await getUsers();
   return (
     <div>
       <h1>Users</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
     </div>
   );
 };
