5bb0f2c2a9703dad404499b052ceb7a171a25d3e
Your Name <you@example.com>
2024-09-16 04:59:07 +0700

    aasdasdasd

--- deploy-docker/lambda/src/main/java/com/learnaws/LambdaHandler.java
+++ deploy-docker/lambda/src/main/java/com/learnaws/LambdaHandler.java
@@ -9,35 +9,31 @@ import java.util.List;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class LambdaHandler implements RequestHandler<Request, Response> {
 
-  private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-  private final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-  private final String queueUrl = "https://sqs.us-east-1.amazonaws.com/033237493133/tri-aws-queue";
-  private final String bucketName = "tri-aws-bucket";
+    private final String queueUrl =
+        "https://sqs.us-east-1.amazonaws.com/033237493133/tri-aws-queue";
+    private final String bucketName = "tri-aws-bucket";
 
-  @Override
-  public Response handleRequest(Request input, Context context) {
-    LambdaLogger logger = context.getLogger();
-    logger.log("Received request: " + input);
-    ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
-    List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
-
-    for (Message message : messages) {
-      String messageBody = message.getBody();
-      String fileName = "message-" + message.getMessageId() + "_" + System.currentTimeMillis() + ".txt";
-      InputStream inputStream = new ByteArrayInputStream(messageBody.getBytes(StandardCharsets.UTF_8));
-      s3.putObject(bucketName, fileName, inputStream, null);
-      sqs.deleteMessage(queueUrl, message.getReceiptHandle());
+    @Override
+    public Response handleRequest(Request input, Context context) {
+        try (S3Client s3Client = S3Client.builder().region(Region.US_EAST_1)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build()) {
+            LambdaLogger logger = context.getLogger();
+            logger.log("Received request: " + input);
+            String fileName = input.requestMessage() + "_" + Instant.now().toString() + ".txt";
+            PutObjectRequest request =
+                PutObjectRequest.builder().bucket(bucketName).key(fileName).build();
+            s3Client.putObject(request,
+                RequestBody.fromString(input.requestMessage() + "_response"));
+            return new Response("Success" + input.requestMessage());
+        }
     }
-    return new Response("Success");
-    
-  }
 }
