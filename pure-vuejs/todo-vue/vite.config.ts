103c744d93424c73411b491e28376b6017fdffa2
Tri Minh To <tri.minhto@hitachids.com>
2024-11-11 16:40:33 +0700

    a

--- pure-vuejs/todo-vue/vite.config.ts
+++ pure-vuejs/todo-vue/vite.config.ts
@@ -2,11 +2,26 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public',
+          dest: ''
+        }
+      ]
+    }),
     Components({
       resolvers: [
         AntDesignVueResolver({
@@ -15,4 +30,5 @@ export default defineConfig({
       ],
     })
   ],
+  publicDir: false,
 });
