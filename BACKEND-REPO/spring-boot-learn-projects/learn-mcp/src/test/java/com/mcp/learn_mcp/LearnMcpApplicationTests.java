c95f8057e8c1639dcc5f33b0d585527fc98d5e62
jt <balisongian@gmail.com>
2025-04-18 21:44:56 +0700

    create learn-mcp spring boot to learn mcp

---
+++ BACKEND-REPO/spring-boot-learn-projects/learn-mcp/src/test/java/com/mcp/learn_mcp/LearnMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.learn_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
