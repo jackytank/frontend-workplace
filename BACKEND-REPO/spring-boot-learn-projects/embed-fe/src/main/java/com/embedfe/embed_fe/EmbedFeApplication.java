39e37e593e5844db01216cc8125321d5738326ed
jt <balisongian@gmail.com>
2025-05-23 22:48:24 +0700

    init spring boot embed fe project

---
+++ BACKEND-REPO/spring-boot-learn-projects/embed-fe/src/main/java/com/embedfe/embed_fe/EmbedFeApplication.java
@@ -0,0 +1,13 @@
+package com.embedfe.embed_fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbedFeApplication.class, args);
+	}
+
+}
---
+++ BACKEND-REPO/spring-boot-learn-projects/embed-fe/src/test/java/com/embedfe/embed_fe/EmbedFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embedfe.embed_fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
