a6797453a5bfafe14d2fb5aa789e5468577ad6fc
Tri Minh To <tri.minhto@hitachids.com>
2025-07-14 14:24:01 +0700

    chore: removed unmaintained projects

--- BACKEND-REPO/spring-boot-learn-projects/cipherian-mongodb/src/main/java/com/cipherian/cipherianmongodb/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cipherian.cipherianmongodb.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.cipherian.cipherianmongodb.model.User;
-
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-
-}
--- BACKEND-REPO/spring-boot-learn-projects/learn-mcp/src/main/java/com/mcp/learn_mcp/constant/AppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.mcp.learn_mcp.constant;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class AppConstant {
-    public static final String BASE_URL = "https://api.weather.gov";
-}
