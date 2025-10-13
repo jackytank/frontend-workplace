698ae4a32f657656947c55fb42861c2256d6de81
jt <balisongian@gmail.com>
2025-10-13 14:19:09 +0700

    research-sql-analyzer

---
+++ spring-boot-learn-projects/research-sql-analyzer/src/main/java/com/research/sql_analyzer/research_sql_analyzer/ResearchSqlAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.research.sql_analyzer.research_sql_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchSqlAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchSqlAnalyzerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-learn-projects/research-sql-analyzer/src/test/java/com/research/sql_analyzer/research_sql_analyzer/ResearchSqlAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.sql_analyzer.research_sql_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchSqlAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
